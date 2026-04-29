d76cb540d0a044b5172adbff88a55d395b305737
fabs <fabiant941@gmail.com>
2026-04-29 00:55:24 -0700

    Initial Commit

---
+++ Hedge Crates/Hedge_CratesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hedge_CratesApp.swift
+//  Hedge Crates
+//
+//  Created by Fabs on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Hedge_CratesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
